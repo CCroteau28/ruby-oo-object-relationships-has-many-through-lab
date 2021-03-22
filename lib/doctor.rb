e4b78ac84979600d40a04434224bae8e96bc400f
CCroteau28 <ccroteau28@gmail.com>
2021-03-21 22:03:08 -0600

    commit tree 63c325acf200aa86d750206af62b9673118b3afa

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,6 +14,10 @@ class Doctor
         @@all
     end
 
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
     def appointments
         Appointment.all.select {|appointment| appointment.doctor == self}
     end
@@ -21,8 +25,5 @@ class Doctor
     def patients
         appointments.map {|appointment| appointment.patient}
     end
-
-    def new_appointment(patient, date)
-        Appointment.new(date, patient, self)
-    end
 end
+  
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 class Patient
-    attr_accessor :name, :appointment, :doctor
+    attr_accessor :name
 
     @@all = []
 
@@ -13,14 +15,14 @@ class Patient
     end
 
     def appointments
-        Appointment.all.select {|appointment| appointment.patient == self}
+       Appointment.all.select {|appointment| appointment.patient == self}
     end
 
     def doctors
         appointments.collect {|appointment| appointment.doctor}
     end
 
-    def new_appointment(doctor, date)
-        Appointment.new(self, date, doctor)
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
     end
 end
